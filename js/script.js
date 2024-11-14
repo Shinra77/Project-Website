2cd8be0267000781bc31d6fb834f4c3d648f94c1
Bagus <bagussetiawan111223@gmail.com>
2024-11-14 14:00:09 +0700

    add navbar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Toggle
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclik = () => {
+    navbarNav.classList.toggle('active')
+};
